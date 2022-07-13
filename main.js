27eb631ea14388fd05ede4a16cc77dbae6efd102
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2022-07-13 14:33:57 +0100

    Removed Class Snake as it was not necessary

--- main.js
+++ main.js
@@ -7,251 +7,229 @@ const buttonRight = document.querySelector("#buttonRight");
 const buttonDown = document.querySelector("#buttonDown");
 const buttonLeft = document.querySelector("#buttonLeft");
 
-class Snake {
+let score = 0;
+let orientation = "";
+let tail = [{
+    positionX: (Math.round(gameArea.getBoundingClientRect().width/16)*16)/2,
+    positionY: (Math.round(gameArea.getBoundingClientRect().height/16)*16)/2
+}]
 
-    score;
+let foodPositionX = 0;
+let foodPositionY = 0;
 
-    orientation;
-    tail;
+const gameAreaHeight = Math.round(gameArea.getBoundingClientRect().height/16)*16;
+const gameAreaWidth = Math.round(gameArea.getBoundingClientRect().width/16)*16;
 
-    gameAreaHeight;
-    gameAreaWidth;
+origin = () => {
 
-    foodPositionX;
-    foodPositionY;
+    //clear everything
+    gameArea.innerHTML = "";
 
-    constructor() {
-        this.score = 0;
-        this.orientation = "";
-        this.tail = [{
-            positionX: (Math.round(gameArea.getBoundingClientRect().width/16)*16)/2,
-            positionY: (Math.round(gameArea.getBoundingClientRect().height/16)*16)/2
-        }]
+    //head of snake
+    gameArea.innerHTML += `<div style="top: ${tail[0].positionY}px; left: ${tail[0].positionX}px" id="snake" class="game__snake"></div>`;
+    
+    document.getElementById("snake").style.top = `${tail[0].positionY}px`;
+    document.getElementById("snake").style.left = `${tail[0].positionX}px`;
 
-        this.gameAreaHeight = Math.round(gameArea.getBoundingClientRect().height/16)*16;
-        this.gameAreaWidth = Math.round(gameArea.getBoundingClientRect().width/16)*16
-
-        this.foodPositionX = 0;
-        this.foodPositionY = 0;
-    }
-
-    origin = () => {
-
-        //clear everything
-        gameArea.innerHTML = "";
-
-        //head of snake
-        gameArea.innerHTML += `<div style="top: ${this.tail[0].positionY}px; left: ${this.tail[0].positionX}px" id="snake" class="game__snake"></div>`;
-        
-        document.getElementById("snake").style.top = `${this.tail[0].positionY}px`;
-        document.getElementById("snake").style.left = `${this.tail[0].positionX}px`;
-
-        this.spawnFood();
-    }
-
-    tailOfSnakeUpdate = () => {
-        //tail of snake
-
-        for (let i=this.tail.length-1; i>0; i--) {
-            this.tail[i].positionY = this.tail[i-1].positionY;
-            this.tail[i].positionX = this.tail[i-1].positionX;
-        }
-
-        const x = document.getElementsByClassName('game__snake-body-part');
-
-        for(let i = 0; i < x.length; i++){
-            x[i].style.top = `${this.tail[i+1].positionY}px`;
-            x[i].style.left = `${this.tail[i+1].positionX}px`;
-        }
-    }
-
-    detectWallCollision = () => {
-        if (this.tail[0].positionY < 0 || this.tail[0].positionY > this.gameAreaHeight-16 || this.tail[0].positionX < 0 || this.tail[0].positionX > this.gameAreaWidth-16) {
-            this.gameOver();
-        }
-    } 
-
-    detectSnakeCollisionWithTail = () => {
-        for (let i=1; i<this.tail.length; i++) {
-            if (this.tail[0].positionX == this.tail[i].positionX && this.tail[0].positionY == this.tail[i].positionY) {
-                this.gameOver();
-            }
-        }
-    }
-
-    spawnFood = () => {
-        //Finds a random coordinate between 0 and the boundary of the game area that is also a multiple of 16
-        const randomFoodPositionY = Math.random() * (this.gameAreaHeight)
-        this.foodPositionY = randomFoodPositionY - (randomFoodPositionY % 16);
-
-        const randomFoodPositionX = Math.random() * (this.gameAreaWidth)
-        this.foodPositionX = randomFoodPositionX - (randomFoodPositionX % 16);
-
-        gameArea.innerHTML += `<div style="top: ${this.foodPositionY}px; left: ${this.foodPositionX}px" id="food" class="game__snake-food"></div>`;
-    }
-
-    spawnTailAfterFood = () => {
-        if (this.tail[0].positionX == this.foodPositionX && this.tail[0].positionY == this.foodPositionY) {
-
-            //Add a tail item to the snake's body
-            //Orientation of snake determines which coordinate the tail should be added
-            if (this.orientation == "N") {
-                this.tail.push({positionX: this.tail[this.tail.length-1].positionX, positionY: this.tail[this.tail.length-1].positionY+16});
-            }
-            else if (this.orientation == "S") {
-                this.tail.push({positionX: this.tail[this.tail.length-1].positionX, positionY: this.tail[this.tail.length-1].positionY-16});
-            }
-            else if (this.orientation == "E") {
-                this.tail.push({positionX: this.tail[this.tail.length-1].positionX-16, positionY: this.tail[this.tail.length-1].positionY});
-            }
-            else if (this.orientation == "W") {
-                this.tail.push({positionX: this.tail[this.tail.length-1].positionX+16, positionY: this.tail[this.tail.length-1].positionY});
-            }
-
-            gameArea.innerHTML += `<div style="top: ${this.tail[1].positionY}px; left: ${this.tail[1].positionX}px" class="game__snake-body-part"></div>`;
-
-            //Update the scoreboard
-            this.score = this.tail.length-1;
-            scoreBoard.innerText = `Score: ${this.score}`;
-
-
-            //Remove the food from the GUI when snake head eats the food
-            const previousFood = document.querySelector("#food");
-            previousFood.remove();
-
-            //Add a new food to the screen
-            this.spawnFood();
-        }
-    }
-
-    gameOver = () => {
-        this.orientation = "";
-        this.tail = [{
-            positionX: this.gameAreaWidth/2,
-            positionY: this.gameAreaHeight/2
-        }]
-
-        this.foodPositionX = 0;
-        this.foodPositionY = 0;
-
-        gameArea.innerHTML = "";
-
-        gameArea.innerHTML = `<h1 class="game__snake-title">Game Over</h1>`;
-
-        setTimeout(() => {this.origin()}, 1000);
-    }
-
-    moveUp = () => {
-        if (this.orientation == "N") {
-
-            //move the snake's body along with the head
-            this.tailOfSnakeUpdate();
-
-            //snake's head UP by 16px 
-            this.tail[0].positionY -= 16;
-
-            document.getElementById("snake").style.top = `${this.tail[0].positionY}px`;
-
-            this.spawnTailAfterFood();
-            this.detectWallCollision();
-            this.detectSnakeCollisionWithTail();
-
-            setTimeout(() => {this.moveUp()}, 200);
-        }
-    }
-
-    moveDown = () => {
-        if (this.orientation == "S") {
-
-            this.tailOfSnakeUpdate();
-
-            //snake's head DOWN by 16px 
-            this.tail[0].positionY += 16;
-
-            document.getElementById("snake").style.top = `${this.tail[0].positionY}px`;
-
-            this.spawnTailAfterFood();
-            this.detectWallCollision();
-            this.detectSnakeCollisionWithTail();
-
-            setTimeout(() => {this.moveDown()}, 200);
-        }
-    }
-
-    moveLeft = () => {
-        if (this.orientation == "W") {
-
-            this.tailOfSnakeUpdate();
-
-            //snake's head LEFT by 16px 
-            this.tail[0].positionX -= 16;
-
-            document.getElementById("snake").style.left = `${this.tail[0].positionX}px`;
-            
-            this.spawnTailAfterFood();
-            this.detectWallCollision();
-            this.detectSnakeCollisionWithTail();
-
-
-            setTimeout(() => {this.moveLeft()}, 200);
-        }
-    }
-
-    moveRight = () => {
-        if (this.orientation == "E") {
-
-            this.tailOfSnakeUpdate();
-
-            //snake's head RIGHT by 16px 
-            this.tail[0].positionX += 16;
-
-            document.getElementById("snake").style.left = `${this.tail[0].positionX}px`;
-
-            this.spawnTailAfterFood();
-            this.detectWallCollision();
-            this.detectSnakeCollisionWithTail();
-
-            setTimeout(() => {this.moveRight()}, 200);
-        }
-    }
-
-
+    spawnFood();
 }
 
-const snakeObject = new Snake();
+tailOfSnakeUpdate = () => {
+    //tail of snake
 
-snakeObject.origin();
+    for (let i=tail.length-1; i>0; i--) {
+        tail[i].positionY = tail[i-1].positionY;
+        tail[i].positionX = tail[i-1].positionX;
+    }
+
+    const x = document.getElementsByClassName('game__snake-body-part');
+
+    for(let i = 0; i < x.length; i++){
+        x[i].style.top = `${tail[i+1].positionY}px`;
+        x[i].style.left = `${tail[i+1].positionX}px`;
+    }
+}
+
+detectWallCollision = () => {
+    if (tail[0].positionY < 0 || tail[0].positionY > gameAreaHeight-16 || tail[0].positionX < 0 || tail[0].positionX > gameAreaWidth-16) {
+        gameOver();
+    }
+} 
+
+detectSnakeCollisionWithTail = () => {
+    for (let i=1; i<tail.length; i++) {
+        if (tail[0].positionX == tail[i].positionX && tail[0].positionY == tail[i].positionY) {
+            gameOver();
+        }
+    }
+}
+
+spawnFood = () => {
+    //Finds a random coordinate between 0 and the boundary of the game area that is also a multiple of 16
+    const randomFoodPositionY = Math.random() * (gameAreaHeight)
+    foodPositionY = randomFoodPositionY - (randomFoodPositionY % 16);
+
+    const randomFoodPositionX = Math.random() * (gameAreaWidth)
+    foodPositionX = randomFoodPositionX - (randomFoodPositionX % 16);
+
+    gameArea.innerHTML += `<div style="top: ${foodPositionY}px; left: ${foodPositionX}px" id="food" class="game__snake-food"></div>`;
+}
+
+spawnTailAfterFood = () => {
+    if (tail[0].positionX == foodPositionX && tail[0].positionY == foodPositionY) {
+
+        //Add a tail item to the snake's body
+        //Orientation of snake determines which coordinate the tail should be added
+        if (orientation == "N") {
+            tail.push({positionX: tail[tail.length-1].positionX, positionY: tail[tail.length-1].positionY+16});
+        }
+        else if (orientation == "S") {
+            tail.push({positionX: tail[tail.length-1].positionX, positionY: tail[tail.length-1].positionY-16});
+        }
+        else if (orientation == "E") {
+            tail.push({positionX: tail[tail.length-1].positionX-16, positionY: tail[tail.length-1].positionY});
+        }
+        else if (orientation == "W") {
+            tail.push({positionX: tail[tail.length-1].positionX+16, positionY: tail[tail.length-1].positionY});
+        }
+
+        gameArea.innerHTML += `<div style="top: ${tail[1].positionY}px; left: ${tail[1].positionX}px" class="game__snake-body-part"></div>`;
+
+        //Update the scoreboard
+        score = tail.length-1;
+        scoreBoard.innerText = `Score: ${score}`;
+
+
+        //Remove the food from the GUI when snake head eats the food
+        const previousFood = document.querySelector("#food");
+        previousFood.remove();
+
+        //Add a new food to the screen
+        spawnFood();
+    }
+}
+
+gameOver = () => {
+    orientation = "";
+    tail = [{
+        positionX: gameAreaWidth/2,
+        positionY: gameAreaHeight/2
+    }]
+
+    foodPositionX = 0;
+    foodPositionY = 0;
+
+    gameArea.innerHTML = "";
+
+    gameArea.innerHTML = `<h1 class="game__snake-title">Game Over</h1>`;
+
+    setTimeout(() => {origin()}, 1000);
+}
+
+moveUp = () => {
+    if (orientation == "N") {
+
+        //move the snake's body along with the head
+        tailOfSnakeUpdate();
+
+        //snake's head UP by 16px 
+        tail[0].positionY -= 16;
+
+        document.getElementById("snake").style.top = `${tail[0].positionY}px`;
+
+        spawnTailAfterFood();
+        detectWallCollision();
+        detectSnakeCollisionWithTail();
+
+        setTimeout(() => {moveUp()}, 200);
+    }
+}
+
+moveDown = () => {
+    if (orientation == "S") {
+
+        tailOfSnakeUpdate();
+
+        //snake's head DOWN by 16px 
+        tail[0].positionY += 16;
+
+        document.getElementById("snake").style.top = `${tail[0].positionY}px`;
+
+        spawnTailAfterFood();
+        detectWallCollision();
+        detectSnakeCollisionWithTail();
+
+        setTimeout(() => {moveDown()}, 200);
+    }
+}
+
+moveLeft = () => {
+    if (orientation == "W") {
+
+        tailOfSnakeUpdate();
+
+        //snake's head LEFT by 16px 
+        tail[0].positionX -= 16;
+
+        document.getElementById("snake").style.left = `${tail[0].positionX}px`;
+        
+        spawnTailAfterFood();
+        detectWallCollision();
+        detectSnakeCollisionWithTail();
+
+
+        setTimeout(() => {moveLeft()}, 200);
+    }
+}
+
+moveRight = () => {
+    if (orientation == "E") {
+
+        tailOfSnakeUpdate();
+
+        //snake's head RIGHT by 16px 
+        tail[0].positionX += 16;
+
+        document.getElementById("snake").style.left = `${tail[0].positionX}px`;
+
+        spawnTailAfterFood();
+        detectWallCollision();
+        detectSnakeCollisionWithTail();
+
+        setTimeout(() => {moveRight()}, 200);
+    }
+}
 
 const dealWithRightMovement = (event) => {
-    if (snakeObject.orientation == "W" || snakeObject.orientation == "E") {
+    if (orientation == "W" || orientation == "E") {
         return;
     }
-    snakeObject.orientation = "E";
-    snakeObject.moveRight();
+    orientation = "E";
+    moveRight();
 }
 
 const dealWithLeftMovement = (event) => {
-    if (snakeObject.orientation == "E" || snakeObject.orientation == "W") {
+    if (orientation == "E" || orientation == "W") {
         return;
     }
-    snakeObject.orientation = "W";
-    snakeObject.moveLeft();
+    orientation = "W";
+    moveLeft();
 }
 
 const dealWithUpMovement = (event) => {
-    if (snakeObject.orientation == "S" || snakeObject.orientation == "N") {
+    if (orientation == "S" || orientation == "N") {
         return;
     }
-    snakeObject.orientation = "N";
-    snakeObject.moveUp();
+    orientation = "N";
+    moveUp();
 }
 
 const dealWithDownMovement = (event) =>{
-    if (snakeObject.orientation == "N" || snakeObject.orientation == "S") {
+    if (orientation == "N" || orientation == "S") {
         return;
     }
-    snakeObject.orientation = "S";
-    snakeObject.moveDown();
+    orientation = "S";
+    moveDown();
 }
 
 const keyHandler = (event) => {
@@ -274,6 +252,8 @@ const keyHandler = (event) => {
             return;
     }    
 }
+
+origin();
 
 body.addEventListener("keydown", keyHandler);
 
